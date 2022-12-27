9c7334b6be689c37561821963110a095c9615844
unknown <mahamuda3052@gmail.com>
2022-12-27 20:19:50 +0600

    create get all posts api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express();
@@ -7,6 +9,36 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.4jfewjr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+const postsColletion = client.db('heroJobTask').collection('posts')
+
+app.post('/posts',async(req,res)=>{
+    const post = req.body;
+    const result = await postsColletion.insertOne(post);
+    res.send(result)
+})
+
+app.get('/posts',async(req,res)=>{
+   
+    let query={}        
+     cursor = postsColletion.find(query)       
+    const posts = await cursor.sort({date : -1}).toArray()
+    res.send(posts)
+})
+
+}
+finally{
+
+}
+}
+run().catch(console.log())
 
 app.get('/',async(req,res)=>{
     res.send('hero task runing')
